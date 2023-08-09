243d37d0a284b0ec08c751b6f6b7706dfd4e31fd
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-08-09 11:41:13 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://montekkundan.github.io/sbr_docs/',
+  url: 'https://montekkundan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sbr_docs/index.html/',
